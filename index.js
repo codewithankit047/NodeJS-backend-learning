0b7e19387c569923d750b6b29342ca28831af14a
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-04-23 22:32:10 +0530

    Mongodb setup

---
+++ index.js
@@ -0,0 +1,32 @@
+// const { MongoClient } = require("mongodb");
+// const url = "mongodb://localhost:27017";
+// let database = "e-comm";
+// const client = new MongoClient(url);
+
+// async function getData() {
+//   let result = await client.connect();
+//   let db = result.db(database);
+//   let collection = db.collection("Product");
+//   let response = await collection.find({}).toArray();
+//   console.log(response);
+// }
+
+// getData();
+
+const {MongoClient} = require('mongodb')
+const url= 'mongodb://localhost:27017';
+const databaseName='new'
+const client= new MongoClient(url);
+
+async function getData()
+{
+    let result = await client.connect();
+    db= result.db(databaseName);
+    collection = db.collection('new');
+    let data = await collection.find({}).toArray();
+    console.log(data)
+
+
+}
+
+getData();
