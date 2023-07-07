2123128fbb5aefaf1560bacd7374856cdcbed8d1
Ankit <111271551+codewithankit047@users.noreply.github.com>
2023-07-07 22:57:55 +0530

    2nd learning

---
+++ Part2/index.jsx
@@ -0,0 +1,10 @@
+// const http = require("http");
+// const fs = require("fs");
+// const server = http.createServer((req, res) => {
+//   console.log("Request has been made  form browser to server");
+// });
+
+// Server.listen(3000, "loclocalhost", () => {
+//   console.log("Server Listing to the 300 Port");
+// });
+
